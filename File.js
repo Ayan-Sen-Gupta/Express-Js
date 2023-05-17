74e5f70c7e8af1cb6a5d3670393d44f92090038f
Ayan-Sen-Gupta <ayansengupta560@gmail.com>
2023-05-17 12:31:40 +0530

    Routers and Filters

--- File.js
+++ File.js
@@ -3,22 +3,18 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin.js');
+const shopRoutes = require('./routes/shop.js');
+
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/add-product',(req,res,next) => {
-   res.send('<form action ="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>');
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
+
+app.use('/', (req,res,next) => { 
+   res.status(404).send('<h1> Page not found </h1>');
 });
 
-app.use('/product',(req,res,next) => {
-   console.log(req.body);
-    res.redirect('/');
-   
-});
-
-
-app.use('/',(req,res,next) => {
-   res.send('<h1> Hello from Express!</h1>');
-});
 
 app.listen(3000);
 
