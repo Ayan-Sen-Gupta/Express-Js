84c0c8c5c66732bde600a5ce95fd175198786028
Ayan-Sen-Gupta <ayansengupta560@gmail.com>
2023-07-05 12:24:42 +0530

    Using Sequelize to connect and sync with database

--- File.js
+++ File.js
@@ -1,28 +1,34 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const errorController = require('./controllers/error.js');
-const db = require('./utilities/database');
 
-const app = express(); 
+const errorController = require('./controllers/error');
+const sequelize = require('./utilities/database');
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin.js');
-const shopRoutes = require('./routes/shop.js');
-const contactRoutes = require('./routes/contact-us.js');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin',adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-app.use(contactRoutes);
 
 app.use(errorController.get404);
 
-
-app.listen(3000);           
+sequelize
+  .sync()
+  .then(result => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });         
 
 
