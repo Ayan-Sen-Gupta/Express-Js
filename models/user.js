f9a6114f8da5dc69cbbfd9e4a0d6d60f5ebbc4f8
Ayan-Sen-Gupta <ayansengupta560@gmail.com>
2023-07-23 14:33:22 +0530

    Designing user and product relationship

--- models/user.js
+++ models/user.js
@@ -1,36 +1,16 @@
 const Sequelize = require('sequelize');
+
 const sequelize = require('../utilities/database');
 
 const User = sequelize.define('user', {
-    id: {
-      type: Sequelize.INTEGER,
-      autoIncrement: true,
-      allowNull: false,
-      primaryKey: true,
-    },
-    name: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: Sequelize.STRING, 
-      allowNull: false,
-      unique: true,
-    },
-    phone: {
-       type: Sequelize.DOUBLE, 
-       allowNull: false, 
-       unique: true,
-    },
-    date: {
-       type: Sequelize.DATEONLY,
-       allowNull: false,
-    },
-    time: {
-        type: Sequelize.TIME,
-        allowNull: false,
-     }
-
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  name: Sequelize.STRING,
+  email: Sequelize.STRING
 });
 
 module.exports = User;
